b44296bb8db7aec3952c513e45ff52f0b435f277
pyother <piotr.dominik.sobol@gmail.com>
2023-06-28 23:38:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# NodeJS and MQTT broker communication 
+
+To run this project forward port no. 8082 and type:
+
+```node
+node app
+```
+
+
+
---
+++ app.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://test.mosquitto.org");
+
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8082); 
+
+client.on("connect", function () {
+  client.subscribe("presence", function (err) {
+    if (!err) {
+      client.publish("presence", "Hello mqtt");
+    }
+  });
+});
+
+client.on("message", function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+  client.end();
+});
