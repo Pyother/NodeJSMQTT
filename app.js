61cab52ab383a75fa034b291d3d2f01fe227f59f
Pyother <piotr.dominik.sobol@gmail.com>
2023-06-30 01:06:20 +0200

    Establish connection with MQTT broker running locally

--- app.js
+++ app.js
@@ -2,16 +2,48 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const mqtt = require("mqtt");
 
+// Ustawienia MQTT
+const brokerUrl = "mqtt://localhost"; // Lokalny adres brokera MQTT
+const topic = "topic";
+
+// Połączenie z brokerem MQTT
+const client = mqtt.connect(brokerUrl);
+
+client.on("connect", () => {
+  console.log("Connected to MQTT broker");
+  client.subscribe(topic);
+});
+
+// Przechowywanie odebranych wiadomości
+const messages = [];
+
+// Obsługa wiadomości MQTT
+client.on("message", (topic, message) => {
+  console.log(`Received message on topic: ${topic}`);
+  console.log(`Message: ${message.toString()}`);
+  
+  // Dodaj wiadomość do tablicy
+  messages.push({
+    topic: topic,
+    message: message.toString()
+  });
+});
+
+// Ustawienie EJS jako widoku
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public'));
 
+// Ustawienie ścieżki dla plików statycznych
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routing dla strony głównej
 app.get('/', (req, res) => {
-  res.render('index'); 
+  res.render('index', { messages: messages }); // Przekazanie tablicy messages do widoku
 });
 
+// Uruchomienie serwera
 app.listen(port, () => {
   console.log(`Area Explorer Dashboard listening on port ${port}`);
 });
