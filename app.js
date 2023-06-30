a126c1fb674b98ba1a187bc502f7ce6a5ca07ea3
Pyother <piotr.dominik.sobol@gmail.com>
2023-06-30 16:12:52 +0200

    Define table displaying all measurements

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express();
 const port = 3000;
 const mqtt = require("mqtt");
 
-// Ustawienia MQTT
-const brokerUrl = "mqtt://localhost"; // Lokalny adres brokera MQTT
-const topic = "topic";
+// MQTT settings
+const brokerUrl = "mqtt://localhost"; 
+const topic = "measurement";
 
-// Połączenie z brokerem MQTT
+// Connection with MQTT broker
 const client = mqtt.connect(brokerUrl);
 
 client.on("connect", () => {
@@ -16,34 +16,73 @@ client.on("connect", () => {
   client.subscribe(topic);
 });
 
-// Przechowywanie odebranych wiadomości
+// Storing messages
 const messages = [];
 
-// Obsługa wiadomości MQTT
+// Handling MQTT messages
 client.on("message", (topic, message) => {
   console.log(`Received message on topic: ${topic}`);
   console.log(`Message: ${message.toString()}`);
-  
-  // Dodaj wiadomość do tablicy
-  messages.push({
-    topic: topic,
-    message: message.toString()
-  });
+
+  const resultArray = splitStringBySpace(message.toString());
+  console.log(resultArray);
+
+  if (topic === topic) {
+    messages.push({
+      degree0: resultArray[0],
+      degree30: resultArray[1],
+      degree60: resultArray[2],
+      degree90: resultArray[3],
+      degree120: resultArray[4],
+      degree150: resultArray[5],
+      degree180: resultArray[6],
+      positionx: resultArray[7],
+      positiony: resultArray[8],
+      density: resultArray[9],
+      time: resultArray[10],
+      message: message.toString()
+    });
+  }
+
 });
 
-// Ustawienie EJS jako widoku
+// Setting ejs as a view
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public'));
 
-// Ustawienie ścieżki dla plików statycznych
+// Setting path for the staticfiles
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routing dla strony głównej
+// Routing for main page
 app.get('/', (req, res) => {
-  res.render('index', { messages: messages }); // Przekazanie tablicy messages do widoku
+  res.render('index', { messages: messages }); // Set messages to the view
 });
 
-// Uruchomienie serwera
+// Run the server
 app.listen(port, () => {
   console.log(`Area Explorer Dashboard listening on port ${port}`);
 });
+
+
+function splitStringBySpace(str) {
+  const result = [];
+
+  let currentVar = '';
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    
+    if (char === ' ') {
+      if (currentVar !== '') {
+        result.push(currentVar);
+        currentVar = '';
+      }
+    } else {
+      currentVar += char;
+    }
+  }
+  if (currentVar !== '') {
+    result.push(currentVar);
+  }
+
+  return result;
+}
